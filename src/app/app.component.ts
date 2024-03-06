58417d02988e062d9fc83132e99adb4b393bb1f5
batyrotich <eddahbaty98@gmail.com>
2024-03-06 11:45:55 +0300

    AfterView init lifeycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -9,16 +9,24 @@ import { CourseCardComponent } from './course-card/course-card.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    console.log("containerDiv, this.card1");
+   
+  }
 
  
 courses= [...COURSES];
 
 @ViewChild('cardRef1', {read:ElementRef})
-card1:CourseCardComponent
+card1:ElementRef;
 
 @ViewChild('container')
 containerDiv:ElementRef;
+
+constructor(){
+  console.log("containerDiv, this.card1");
+}
 
   onCourseSelected(course:Course){
     console.log("containerDiv, this.containerDiv");
