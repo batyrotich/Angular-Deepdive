cbb0710fe3aa3549eab13130649077d6e12756ba
batyrotich <eddahbaty98@gmail.com>
2024-03-06 10:48:21 +0300

    Pipes syntax in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,11 @@ export class AppComponent {
  
 courses= [...COURSES];
 
+startDate= new Date(2000,0,1);
+title = COURSES[0].description;
+price = 9.99778754333;
+rate=0.67;
+course= COURSES[0];
   onCourseSelected(course:Course){
     console.log("App component - click event bubbled..", course);
   }
